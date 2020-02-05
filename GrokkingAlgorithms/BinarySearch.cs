71786885d9d4d0338609113cae8b3a54c990296d
Zhivula <56290740+Zhivula@users.noreply.github.com>
2020-02-05 14:18:21 +0300

    Бинарные поиск

---
+++ GrokkingAlgorithms/BinarySearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrokkingAlgorithms
+{
+    class BinarySearch
+    {
+        public static bool TryGetResult(IList<int> list, int element)
+        {
+            if (list != null)
+            {
+                var low = 0;
+                var high = list.Count()-1;
+                var middle = (list.Count()-1) / 2;
+                while (low<=high)
+                {
+                    if (list[middle]==element)
+                    {
+                        return true;
+                    }
+                    else if(list[middle] > element)
+                    {
+                        high = middle-1;
+                        middle = (high + low) / 2;
+                    }
+                    else
+                    {
+                        low = middle+1;
+                        middle = (low + high) / 2;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
--- GrokkingAlgorithms/Program.cs
+++ GrokkingAlgorithms/Program.cs
@@ -10,6 +10,22 @@ namespace GrokkingAlgorithms
     {
         static void Main(string[] args)
         {
+            List<int> list = new List<int>() {1, 2,3,4,5,6,7,8,9};
+            ConsoleKey key = ConsoleKey.Enter;
+            while (key != ConsoleKey.Escape)
+            {
+                Console.WriteLine("Введите, пожалуйста, число: ");
+                var userInput = int.Parse(Console.ReadLine());
+                bool result = BinarySearch.TryGetResult(list, userInput);
+                Console.WriteLine("Результат работы бинарного поиска: " + result);
+                if (result == true) Console.WriteLine("Элемент " + userInput + " был найден!");
+                else Console.WriteLine("Элемент " + userInput + " не был найден!");
+
+                Console.WriteLine("Для повтора операции нажмите любую клавишу, а для завершения работы программы \'Esc\'");
+                key = Console.ReadKey().Key;
+            }
+            Console.WriteLine("Программа закончила выполнение.");
+            Console.ReadKey();
         }
     }
 }
