d705e333a68d70d4f995674898d503b0ef2954d4
Zhivula <56290740+Zhivula@users.noreply.github.com>
2020-02-08 20:36:19 +0300

    Быстрая сортировка

---
+++ GrokkingAlgorithms/QuickSort.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrokkingAlgorithms
+{
+    class QuickSort
+    {
+        public int[] Sort(int[] array)
+        {
+            return QuickSortMethod(array, 0, array.Length - 1);
+        }
+        //метод для обмена элементов массива
+        static void Swap(ref int x, ref int y)
+        {
+            var t = x;
+            x = y;
+            y = t;
+        }
+
+        //метод возвращающий индекс опорного элемента
+        static int Partition(int[] array, int minIndex, int maxIndex)
+        {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+            return pivot;
+        }
+
+        //быстрая сортировка
+        static int[] QuickSortMethod(int[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            var pivotIndex = Partition(array, minIndex, maxIndex);
+            QuickSortMethod(array, minIndex, pivotIndex - 1);
+            QuickSortMethod(array, pivotIndex + 1, maxIndex);
+
+            return array;
+        }
+    }
+}
